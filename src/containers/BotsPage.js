8ede5d9b92d50067b50b0d9f108b3a4b9907efa8
Brian Horowitz <guitarrook94@gmail.com>
2019-01-16 11:44:09 -0500

    Code-Challenge

--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,12 +1,46 @@
 import React from "react";
+import BotCollection from '../containers/BotCollection'
+import YourBotArmy from './YourBotArmy'
 
 class BotsPage extends React.Component {
   //start here with your code for step one
 
+  state = {
+    bots: [],
+    clickedBot: []
+  }
+
+  getBots() {
+    fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+    .then(res => res.json())
+    .then(data => {
+      this.setState({
+        bots: data
+      })
+    })
+  }
+
+  componentDidMount() {
+    this.getBots();
+  }
+
+  handleClick = event => {
+    console.log('I work!')
+    this.setState({
+      clickedBot: event.target.value
+    })
+  }
+
   render() {
     return (
       <div>
-        {/* put your components here */}
+      <h1>Bots Page</h1>
+      <YourBotArmy
+      bots = {this.state.clickedBot}/>
+      <BotCollection
+      bots = {this.state.bots}
+      handleClick = {this.handleClick}
+       />
       </div>
     );
   }
--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -2,21 +2,38 @@ import React from "react";
 import BotCard from "../components/BotCard";
 
 class YourBotArmy extends React.Component {
-  //your bot army code here...
+
+
+  state = {
+    botArmy: []
+  }
+
+  // handleClick = event => {
+  //   this.setState({
+  //     clicked: !this.state.clicked
+  //   })
+  // }
+
 
   render(){
     return (
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {/*...and here...*/}
+            {this.props.bots.map(robot => {
+              return <BotCard
+              bot = { robot }
+              // clicked = {this.state.clicked}
+              // handleClick = {this.handleClick}
+              />
+            })}}
             Your Bot Army
           </div>
         </div>
       </div>
     );
   }
-  
+
 };
 
 export default YourBotArmy;
