963da669b6a6264f82ace373a7456b17140004d8
jessanderson222 <jessanderson222@gmail.com>
2019-01-16 10:12:54 -0500

    botList rendered

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -4,17 +4,17 @@ import BotCard from "../components/BotCard";
 class BotCollection extends React.Component {
   //your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  render() {
+    console.log(this.props);
+    let botList = this.props.bots.map((bot, i) => (
+      <BotCard key={i} bot={bot} />
+    ));
+    return (
+      <div className="ui four column grid">
+        <div className="row">{botList}</div>
+      </div>
+    );
   }
-
-};
+}
 
 export default BotCollection;
