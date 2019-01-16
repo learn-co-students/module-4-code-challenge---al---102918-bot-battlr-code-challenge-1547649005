4c9ab8d11c06d9376ffd12c359b27eacbcfcec76
github username <github email address>
2019-01-16 11:15:48 -0500

    shows the view page on click

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,21 +1,53 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from "../components/BotSpecs"
 
 class BotCollection extends React.Component {
-  //your code here
+
+  state={
+    view: "all",
+    bot: {}
+  }
+
+  singleView= (viewBot)=> {
+    this.setState({
+      view: "single",
+      bot: viewBot
+    })
+  }
+
+  goBack = (viewBot) => {
+    this.setState({
+      view: "all"
+    })
+  }
 
   render(){
     console.log(this.props)
     let botList = this.props.botList.map((singleBot)=> {
-      return <BotCard bot={singleBot} key={singleBot.id} clickHandler={this.props.clickHandler}/>
+      return <BotCard bot={singleBot} key={singleBot.id} clickHandler={this.singleView}/>
     })
+    if (this.state.view === "all"){
   	return (
   	  <div className="ui four column grid">
     		<div className="row">
           {botList}
     		</div>
   	  </div>
-  	);
+  	);}
+    else {
+    return (
+  	  <div className="ui four column grid">
+    		<div className="row">
+          <BotSpecs
+           bot={this.state.bot}
+           clickHandler={this.props.clickHandler}
+           goBack={this.goBack}
+            />
+    		</div>
+  	  </div>
+  	)
+    }
   }
 
 };
